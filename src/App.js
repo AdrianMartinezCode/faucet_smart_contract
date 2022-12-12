85011748b7269fdfbd4914d6f1b72dda6c33fb4a
AdrianMartinezCode <amartinezm1994@gmail.com>
2022-12-12 08:46:23 +0100

    69th lesson

--- src/App.js
+++ src/App.js
@@ -1,8 +1,60 @@
+import { useEffect, useState } from "react";
+import Web3 from "web3";
+import "./App.css";
 
 function App() {
+
+  const [web3Api, setWeb3Api] = useState({
+    provider: null,
+    web3: null
+  });
+
+  // use effect will be used once time when the component is loaded
+  useEffect(() => {
+    const loadProvider = async () => {
+      // with metamask we have an access to window.ethereum & to window.web3
+      // metamask injects a global API into website
+      // this API allows websites to request users, accounts, read data to blockchain
+      // sign messages and transactions
+
+      console.log(window.web3);
+      console.log(window.ethereum);
+
+      let provider = null;
+
+      if (window.ethereum) {
+        provider = window.ethereum;
+        try {
+          await provider.enable();
+        } catch {
+          console.error("User denied accounts access!");
+        }
+      } else if (window.web3) {
+        provider = window.web3.currentProvider;
+      } else if (!process.env.production) {
+        provider = new Web3.providers.HttpProvider("http://localhost:7545");
+      }
+
+      setWeb3Api({
+        web3: new Web3(provider),
+        provider
+      })
+    }
+
+    loadProvider();
+  }, []);
+
+  console.log(web3Api.web3);
+
   return (
-    <div className="App">
-      Hello world
+    <div className="faucet-wrapper">
+      <div className="faucet">
+        <div className="balance-view is-size-2">
+          Current Balance: <strong>10</strong> ETH
+        </div>
+        <button className="btn mr-2">Donate</button>
+        <button className="btn">Withdraw</button>
+      </div>
     </div>
   );
 }
